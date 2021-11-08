95ce93e16bc292bc3a616fd45c0649a9febb7cbd
jiangpeng jiang <jiangpeng.jiang@zhaopin.com.cn>
2021-11-08 14:07:47 +0800

    update

---
+++ src/main/scala/graph_embedding/Main.scala
@@ -0,0 +1,70 @@
+package graph_embedding
+
+import org.apache.spark.ml.feature.Word2Vec
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object Main {
+
+	def main(args: Array[String]): Unit = {
+
+		val options = Array(
+			"dataPath",
+			"embeddingPath",
+			"indexPath",
+			"vectorSize",
+			"windowSize",
+			"maxIter",
+			"topK",
+			"numWalk",
+			"walkLength",
+			"masterURL"
+		)
+
+		val argParser = ArgsParser.get(args, options)
+
+		val dataPath = argParser.getString("dataPath")
+		val embeddingPath = argParser.getString("embeddingPath")
+		val indexPath = argParser.getString("indexPath")
+		val vectorSize = argParser.getInt("vectorSize")
+		val windowSize = argParser.getInt("windowSize")
+		val numWalk = argParser.getInt("numWalk")
+		val walkLength = argParser.getInt("dataPath")
+		val masterURL = argParser.getString("masterURL")
+		val maxIter = argParser.getInt("maxIter")
+
+		val spark = SparkSession.builder().master(masterURL).getOrCreate()
+
+		val data = DataLoader.loadEdgeList(spark, dataPath)
+
+		val processedDF = GraphUtil.preProcess(data)
+
+		val walk = new DeepWalk()
+			.setNumWalk(numWalk)
+			.setWalkLength(walkLength)
+
+		val result = walk.randomWalk(processedDF).cache()
+		result.first()
+
+		val w2v = new Word2Vec()
+			.setMaxSentenceLength(100)
+			.setMinCount(2)
+			.setWindowSize(windowSize)
+			.setVectorSize(vectorSize)
+			.setMaxIter(maxIter)
+			.setNumPartitions(30)
+			.setInputCol("sequence")
+			.fit(result)
+
+		var wv = w2v.getVectors.toDF("word", "vector")
+
+		wv.write.mode("overwrite").save(embeddingPath)
+
+		wv = wv.withColumn("vector", Word2VecIndexGenerator.vecToSeq(col("vector")))
+
+		val index = Word2VecIndexGenerator.generateIndex(20, wv, wv, "word", "vector", "word", "vector")
+
+		index.write.mode("overwrite").save(indexPath)
+
+	}
+}
---
+++ src/main/scala/graph_embedding/graph/package.scala
@@ -0,0 +1,13 @@
+package graph_embedding
+
+package object graph {
+
+	case class NodeAttr(var neighbors: Array[(Long, Double)] = Array.empty[(Long, Double)],
+											var J: Array[Int] = Array.empty[Int],
+											var q: Array[Double] = Array.empty[Double]) extends Serializable
+
+	case class EdgeAttr(var dstNeighbors: Array[Long] = Array.empty[Long],
+											var J: Array[Int] = Array.empty[Int],
+											var q: Array[Double] = Array.empty[Double]) extends Serializable
+
+}
